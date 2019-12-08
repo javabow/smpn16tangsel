41252462f31735b0009d37622fe23d8dd898639f
Jang <jabbarujang@gmail.com>
2019-12-08 10:45:33 +0700

    h

--- js/multi-location.js
+++ js/multi-location.js
@@ -1,8 +1,20 @@
 $(document).on('change', '#loclen', function() {
   thisval = $(this).val();
-  $('#loc-container').html('');
-  for (var i = 1; i <= thisval; i++) {
-    html = '<br><p>Lokasi '+i+'</p> <label>Nama Lokasi</label> <input class="form-control" type="text" name="location_name[]" > <label>Alamat Lokasi</label> <input class="form-control" type="text" name="location_address[]" > <label>Latitude</label> <input class="form-control" type="number" name="lat[]" step="any"> <label>Longitude</label> <input class="form-control" type="number" name="lng[]" step="any">';
-    $('#loc-container').append(html);
+  oldLocContainer = $('#loc-container').html();
+  len = $('#loc-container').find('input[name="location_name[]"]').length;
+  selisih = thisval - len;
+
+  if (selisih > 0) {
+    for (var i = len+1; i <= thisval; i++) {
+      html = '<div class="ss'+i+'"><p>Lokasi '+i+'</p> <label>Nama Lokasi</label> <input class="form-control" type="text" name="location_name[]" > <label>Alamat Lokasi</label> <input class="form-control" type="text" name="location_address[]" > <label>Latitude</label> <input class="form-control" type="number" name="lat[]" step="any"> <label>Longitude</label> <input class="form-control" type="number" name="lng[]" step="any"><br></div> ';
+      $('#loc-container').append(html);
+    }
+  } else {
+    // $('#loc-container').html('');
+    for (var i = len; i > len+selisih; i--) {
+      // html = '<br><p>Lokasi '+i+'</p> <label>Nama Lokasi</label> <input class="form-control" type="text" name="location_name[]" > <label>Alamat Lokasi</label> <input class="form-control" type="text" name="location_address[]" > <label>Latitude</label> <input class="form-control" type="number" name="lat[]" step="any"> <label>Longitude</label> <input class="form-control" type="number" name="lng[]" step="any">';
+      $('#loc-container').find('.ss'+i).remove();
+    }
   }
+
 });
